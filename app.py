16d22c7d7a75b967068b4cede7e0c7a07ba48e6c
Saksham Singla <sakshamsingla21@gmail.com>
2024-06-26 17:20:43 +0530

    changed

--- app.py
+++ app.py
@@ -106,5 +106,5 @@ def ask_question():
         return jsonify({"reply": response})
     return jsonify({"error": "Question not provided"}), 400
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
